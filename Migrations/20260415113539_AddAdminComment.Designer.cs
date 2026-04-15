de8f95bd150e42b34ad1c78ca83de36e30932c6d
gabdullina777-cell <gabdullina777@gmail.com>
2026-04-15 16:51:28 +0500

    Add migration files

--- Migrations/20260415113539_AddAdminComment.Designer.cs
+++ Migrations/20260415113539_AddAdminComment.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ayul_dayusy.Models;
 
@@ -10,9 +11,11 @@ using ayul_dayusy.Models;
 namespace ayul_dayusy.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260415113539_AddAdminComment")]
+    partial class AddAdminComment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
---
+++ Migrations/20260415113539_AddAdminComment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ayul_dayusy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdminComment",
+                table: "Petitions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminComment",
+                table: "Petitions");
+        }
+    }
+}
